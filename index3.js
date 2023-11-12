b7eb2e1a7d9af23ae1819b3ab49c2b4b43c37dee
Savindu@Pasintha <64083148+savindu-pasintha@users.noreply.github.com>
2023-11-12 22:24:31 +0530

    replace file

---
+++ index3.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Home Page Route'));
+
+app.get('/about', (req, res) => res.send('About Page Route'));
+
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
